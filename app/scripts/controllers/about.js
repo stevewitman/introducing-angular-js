988f37e53c152d3ee9a2edadde7058958b6deefe
Steve Witman <stevewitman@gmail.com>
2015-07-19 14:05:34 -0600

    began with yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name movieManiaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the movieManiaApp
+ */
+angular.module('movieManiaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name movieManiaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the movieManiaApp
+ */
+angular.module('movieManiaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
